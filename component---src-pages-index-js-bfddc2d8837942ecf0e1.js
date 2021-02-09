70221524648a52a761117e0159a7589641677f30
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2021-02-09 12:36:09 +0530

    Updates

---
+++ component---src-pages-index-js-bfddc2d8837942ecf0e1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement("h1",null,"Hello there! "),l.a.createElement("p",null,"Data to be added here..."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-bfddc2d8837942ecf0e1.js.map
---
+++ component---src-template-blog-js-9666edca07cf3070ecce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{scXd:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),l=n("Bl7J");t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement("p",null,t.frontmatter.date),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
+//# sourceMappingURL=component---src-template-blog-js-9666edca07cf3070ecce.js.map
